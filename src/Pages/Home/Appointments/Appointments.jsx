0240d88735b5e085358f8ecf197c92e2679d5152
Assraf Nur <92791009+assraf-nur@users.noreply.github.com>
2023-05-08 22:37:33 +0600

    banner done

--- src/Pages/Home/Appointments/Appointments.jsx
+++ src/Pages/Home/Appointments/Appointments.jsx
@@ -1,7 +1,17 @@
-import React from 'react'
+import React from "react";
+import doctor from '../../../Assets/images/doctor.png'
 
 export default function Appointments() {
   return (
-    <div>Appointments</div>
-  )
+    <div className="bg-cover bg-center h-screen appointment-page">
+      <div className="hero min-h-screen">
+        <div className="hero-content flex-col lg:flex-row-reverse">
+          <img alt="" src={doctor} className="max-w-xl shadow-2xl rounded-xl" />
+          <div>
+            
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
